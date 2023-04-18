94d0c1c8b4290b2c76ad7f9d7b7154eac066ec5a
Warghto <vasilijtsaryk@gmail.com>
2023-04-18 23:47:43 +0200

    nothing

--- marvel_starter/src/components/app/App.js
+++ marvel_starter/src/components/app/App.js
@@ -2,10 +2,24 @@ import AppHeader from "../appHeader/AppHeader";
 import RandomChar from "../randomChar/RandomChar";
 import CharList from "../charList/CharList";
 import CharInfo from "../charInfo/CharInfo";
+import axios from "axios";
 
 import decoration from '../../resources/img/vision.png';
 
 const App = () => {
+    axios.get('https://gateway.marvel.com/v1/public/characters', {
+        params: {
+            apikey: 'yourapikey',
+        }
+    })
+        .then(response => {
+            console.log(response.data);
+        })
+        .catch(error => {
+            console.log(error);
+        });
+
+
     return (
         <div className="app">
             <AppHeader/>
