789357aee114465d7e6a20fba13471244dbbacc5
Avoid <vasilijtsaryk@gmail.com>
2023-04-19 05:58:20 +0000

    axios get

--- marvel_starter/src/components/app/App.js
+++ marvel_starter/src/components/app/App.js
@@ -4,35 +4,35 @@ import CharList from "../charList/CharList";
 import CharInfo from "../charInfo/CharInfo";
 import axios from "axios";
 
-import decoration from '../../resources/img/vision.png';
+import decoration from "../../resources/img/vision.png";
 
 const App = () => {
-    axios.get('https://gateway.marvel.com/v1/public/characters', {
-        params: {
-            apikey: 'yourapikey',
-        }
+  axios
+    .get("https://gateway.marvel.com/v1/public/characters", {
+      params: {
+        apikey: "apikey=9ad12a117e62271964992d7020300d8d",
+      },
     })
-        .then(response => {
-            console.log(response.data);
-        })
-        .catch(error => {
-            console.log(error);
-        });
+    .then((res) => {
+      console.log(res.data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 
-
-    return (
-        <div className="app">
-            <AppHeader/>
-            <main>
-                <RandomChar/>
-                <div className="char__content">
-                    <CharList/>
-                    <CharInfo/>
-                </div>
-                <img className="bg-decoration" src={decoration} alt="vision"/>
-            </main>
+  return (
+    <div className="app">
+      <AppHeader />
+      <main>
+        <RandomChar />
+        <div className="char__content">
+          <CharList />
+          <CharInfo />
         </div>
-    )
-}
+        <img className="bg-decoration" src={decoration} alt="vision" />
+      </main>
+    </div>
+  );
+};
 
 export default App;
